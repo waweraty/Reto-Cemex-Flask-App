1afa365c9e6a2c41de2965f73ae34c810d6d1c8f
waweraty <waweraty@users.noreply.github.com>
2021-03-10 12:06:14 -0600

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,27 @@ import numpy as np
 import os
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.multioutput import MultiOutputRegressor
-from RegresorClass import Regresor
 
 #TEMPLATE = '/templates'
 #STATIC = '/static'
+
+class Regresor:
+    def __init__(self, regresor):
+        self.regressor = regresor
+        
+    def calculaOptimo(self, datos_entrada):
+        datos = np.array(datos_entrada)
+        datos = datos.reshape(1, -1)
+        datos_salida = self.regressor.predict(datos)
+        return datos_salida
+    
+    def imprimeOptimo(self, datos_entrada):
+        datos = np.array(datos_entrada)
+        datos = datos.reshape(1, -1)
+        EE = self.regressor.predict(datos)[0][0]
+        EC = self.regressor.predict(datos)[0][1]
+        cost = self.regressor.predict(datos)[0][2]
+        print('EE: %s\nEC: %s\nCosto Ponderado unitario: %s'% (EE,EC,cost))
 
 #, template_folder=TEMPLATE, static_folder=STATIC
 app = Flask(__name__)
